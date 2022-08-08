a858e2dc1e54ca8af402e194ca3e2fa745cef517
Wayne Schuller <wayne@schuller.id.au>
2022-08-08 19:27:13 +1000

    Significant refactoring. Big chart, welcome page.

--- strength_tracker.js
+++ strength_tracker.js
@@ -16,159 +16,192 @@ function onOpen() {
     ui.createMenu('Strength Tracker')
         .addItem('Process BLOC CSV data', 'process_BLOC_data')
         .addToUi();
+    
+    // Check for Welcome sheet.
+    let sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Welcome");
+    if (!sheet) create_welcome_sheet();
 }
 
+// Always have the first sheet as a welcome sheet with instructions and a place to put config URLs
+function create_welcome_sheet() {
+    console.log("Creating welcome sheet");
+    var spreadsheet = SpreadsheetApp.getActive();
+    spreadsheet.getRange('A1').activate();
+    spreadsheet.insertSheet(1);
+    spreadsheet.getCurrentCell().setValue('Welcome');
+    spreadsheet.getRange('A2').activate();
+    spreadsheet.getCurrentCell().setValue('Enter full URL for your BLOC CSV file on Google Drive:');
+    spreadsheet.getRange('A3').activate();
+    spreadsheet.getActiveSheet().setColumnWidth(1, 382);
+    spreadsheet.getActiveSheet().setColumnWidth(2, 325);
+    spreadsheet.getRange('A1').activate();
+    spreadsheet.getActiveRangeList().setFontWeight('bold');
+    spreadsheet.getActiveSheet().setName('Welcome');
+    spreadsheet.moveActiveSheet(1);
+}
 
 // Find the BLOC data and iterate row by row and collect data with top lifts for each session
+// FIXME: try to handle any errors gracefully with a dialog explanation
 function process_BLOC_data() {
     
     let outputValues = []; // we will store our collected data here
-    let ss = SpreadsheetApp.getActiveSpreadsheet(); // FIXME: whats the cool way to check for null etc
 
-    // Loop through all the sheets in the spreadsheet looking for the BLOC data 
-    let allsheets = ss.getSheets();
+    let ss = SpreadsheetApp.getActiveSpreadsheet();
+    let csv_url = ss.getRange('Welcome!B2').getValue();
+
+    let bloc_csv_ss = SpreadsheetApp.openByUrl(csv_url);
+    
+    // Get the first sheet
+    let sheet = bloc_csv_ss.getSheets()[0];
 
     let bloc_found = false;
 
-    for (let s in allsheets) {
-        let sheet = allsheets[s];
-        let values = sheet.getDataRange().getValues(); //FIXME - fails on a sheet with no values
-    
-        // Dynamically find where the columns are
-        // We do not assume that BLOC uses consistent column order.
-        let workout_date_COL, completed_COL, exercise_name_COL, assigned_reps_COL, assigned_weight_COL, actual_reps_COL, actual_weight_COL, missed_COL;
+    if (!sheet) return;
 
-        for (let col = 0; col < values[0].length; col++) {
+    if (sheet.getType() === SpreadsheetApp.SheetType.OBJECT) return;  // Ignore these own-chart sheets
 
-            // Logger.log("Title row. Column name is: %s", values[0][col]);
-                
-            switch (values[0][col]) {
-                case DATEFIELD:
-                    workout_date_COL = col;
-                    break;
-                case COMPLETED:
-                    completed_COL = col;
-                    break;
-                case EXERCISENAME:
-                    exercise_name_COL = col;
-                    break;
-                case ASSIGNEDREPS:
-                    assigned_reps_COL = col;
-                    break;
-                case ASSIGNEDWEIGHT:
-                    assigned_weight_COL = col;
-                    break;  
-                case ACTUALREPS:
-                    actual_reps_COL = col;
-                    break;
-                case ACTUALWEIGHT:
-                    actual_weight_COL = col;
-                    break;
-                case MISSED:
-                    missed_COL = col;
-            }
-    
-            // Give up on this sheet if we did not find all our expected BLOC data column names
-            if (!workout_date_COL ||
-                !completed_COL ||
-                !exercise_name_COL ||
-                !assigned_reps_COL ||
-                !assigned_weight_COL ||
-                !actual_reps_COL ||
-                !actual_weight_COL) 
-            continue; 
+    let values = sheet.getDataRange().getValues(); 
+   
+    // Iterate through the header row and learn where the columns are
+    // We do not assume that BLOC uses consistent column order.
+    let workout_date_COL, completed_COL, exercise_name_COL, assigned_reps_COL, assigned_weight_COL, actual_reps_COL, actual_weight_COL, missed_COL;
+    for (let col = 0; col < values[0].length; col++) {
 
-            /* Iterate backwards in chronological order - skip row 0 with titles */
-            for (let row = values.length - 1 ; row > 1; row--) {
+        // Logger.log("Title row. Column name is: %s", values[0][col]);
+        switch (values[0][col]) {
+            case DATEFIELD:
+                workout_date_COL = col;
+                break;
+            case COMPLETED:
+                completed_COL = col;
+                break;
+            case EXERCISENAME:
+                exercise_name_COL = col;
+                break;
+            case ASSIGNEDREPS:
+                assigned_reps_COL = col;
+                break;
+            case ASSIGNEDWEIGHT:
+                assigned_weight_COL = col;
+                break;  
+            case ACTUALREPS:
+                actual_reps_COL = col;
+                break;
+            case ACTUALWEIGHT:
+                actual_weight_COL = col;
+                break;
+            case MISSED:
+                missed_COL = col;
+        }
+    }
+
+    // Give up if we did not find all our expected BLOC data column names
+    if (!workout_date_COL ||
+        !completed_COL ||
+        !exercise_name_COL ||
+        !assigned_reps_COL ||
+        !assigned_weight_COL ||
+        !actual_reps_COL ||
+        !actual_weight_COL) 
+        return; 
       
-            // Give up on this row if there is no date field (should never happen)
-            if (!values[row][workout_date_COL]) continue;
+    console.log("Excellent BLOC data set header row.");
 
-            // Give up on this row if it is not a completed workout
-            if (!values[row][completed_COL]) continue; 
-       
-            // Give up on this row if there is no assigned reps 
-            // Normally this is just coach programming comments in the BLOC data
-            if (!values[row][assigned_reps_COL]) continue;
+    /* Iterate CSV rows in reverse - skip row 0 with titles */
+    for (let row = values.length - 1 ; row > 0; row--) {
       
-            // Give up on this row if it was marked as missed TRUE
-            if (values[row][missed_COL]) continue;
+        // Give up on this row if there is no date field (should never happen)
+        if (!values[row][workout_date_COL]) continue;
 
-            // Chose which column of processed data to place results
-            let col = 5; // throw all spare lifts into column 5 of the new sheet
-            switch (values[row][exercise_name_COL]) {
-                case "Squat":
-                    col = 1;
-                    break;
-                case "Bench Press":
-                    col = 2;
-                    break;
-                case "Deadlift":
-                    col = 3;
-                    break;
-                case "Press":
-                    col = 4;
-                    break;
-                }
-
-            // FIXME: only process Squats for now to get contiguous data 
-            if (col != 1) continue;
-
-            // Record assigned reps and weight.
-            var lifted_reps = values[row][assigned_reps_COL];
-            var lifted_weight = values[row][assigned_weight_COL];
+        // Give up on this row if it is not a completed workout
+        if (!values[row][completed_COL]) continue; 
        
-            // Logger.log("Found date: %s, %s reps at %s kg.", values[row][workout_date_COL], lifted_reps, lifted_weight);
+        // Give up on this row if there is no assigned reps 
+        // Happens when coach leaves comments in the app
+        if (!values[row][assigned_reps_COL]) continue;
+      
+        // Give up on this row if it was marked as missed TRUE
+        if (values[row][missed_COL]) continue;
 
-            // Override if there is an actual_reps and actual_weight as well
-            // This happens when the person lifts different to what was assigned by their coach
-            if (values[row][actual_reps_COL] && values[row][actual_weight_COL]) {
-                lifted_reps = values[row][actual_reps_COL];
-                lifted_weight = values[row][actual_weight_COL];
-            }
+        // Chose which column of processed data to place results
+        let col = 5; // throw all spare lifts into column 5 of the new sheet
+        switch (values[row][exercise_name_COL]) {
+            case "Squat":
+                col = 1;
+                break;
+            case "Bench Press":
+                col = 2;
+                break;
+            case "Deadlift":
+                col = 3;
+                break;
+            case "Press":
+                col = 4;
+                break;
+        }
 
-            // Calculate 1RM for the list lift
-            let onerepmax = estimateE1RM(lifted_reps, lifted_weight);
+        // Record assigned reps and weight.
+        var lifted_reps = values[row][assigned_reps_COL];
+        var lifted_weight = values[row][assigned_weight_COL];
+     
+        // Logger.log("Found date: %s, %s reps at %s kg.", values[row][workout_date_COL], lifted_reps, lifted_weight);
+
+        // Override if there is an actual_reps and actual_weight as well
+        // This happens when the person lifts different to what was assigned by their coach
+        if (values[row][actual_reps_COL] && values[row][actual_weight_COL]) {
+            lifted_reps = values[row][actual_reps_COL];
+            lifted_weight = values[row][actual_weight_COL];
+        }
+
+        // Calculate 1RM for the set
+        let onerepmax = estimateE1RM(lifted_reps, lifted_weight);
                         
-            // Logger.log("onerep max is: %s", onerepmax);
             
-            // Iterate on the collected data so far to see if we have this date already
-            let datefound = false;
-            for (let j in outputValues) {
-                if (outputValues[j][0].toDateString() == values[row][workout_date_COL].toDateString()) {
-                    if (onerepmax > outputValues[j][col]) {
-                        outputValues[j][col] = onerepmax;
-                    }
-                datefound = true;
+        // Iterate on the collected data so far to see if we have this date already
+        let datefound = false;
+        for (let j in outputValues) {
+            if (outputValues[j][0].toDateString() == values[row][workout_date_COL].toDateString()) {
+                if (onerepmax > outputValues[j][col]) {
+                    outputValues[j][col] = onerepmax;
                 }
+            datefound = true;
             }
+        }
 
-            // If this is a new date then create a new row in our collected dataset here.
-            if (!datefound) {
-                let newrow = [values[row][workout_date_COL], "", "", "", "", ""];
-                newrow.fill(onerepmax, col, col+1);
-                outputValues.push(newrow);
-              }
-            }
+        // If this is a new date then create a new row in our collected dataset here.
+        if (!datefound) {
+            let newrow = [values[row][workout_date_COL], "", "", "", "", ""];
+            newrow.fill(onerepmax, col, col+1);
+            outputValues.push(newrow);
+            bloc_found = true;  // Is this really needed?
+        }
+    }
   
-        // Output the collected data to a new sheet
-        let outputsheet = ss.insertSheet(); 
+    // Output the collected data to a special sheet
+    let outputsheet = ss.getSheetByName("Processed E1RMs");
+    if (outputsheet) ss.deleteSheet(outputsheet);
+    outputsheet = ss.insertSheet().setName("Processed E1RMs");
 
-        ss.appendRow(["Date","Squat","Bench","Deadlift","Press","Other"]); // Headings
-        outputsheet.getRange(1, 1, 1, 6).setFontWeight("bold");
+      
+    ss.appendRow(["Date","Squat","Bench","Deadlift","Press","Other"]); // Headings
+    outputsheet.getRange(1, 1, 1, 6).setFontWeight("bold");
 
-        // Output data into sheet starting in row 2 (row 1 is headings)
-        outputsheet.getRange(2, 1, outputValues.length, outputValues[0].length).setValues(outputValues);
+    // Output data into sheet starting in row 2 (row 1 is headings)
+    outputsheet.getRange(2, 1, outputValues.length, outputValues[0].length).setValues(outputValues);
         
-        // Draw a basic Squat progress chart
-        let chartSquatRange = outputsheet.getRange('A:B');
-        let lineChartBuilder = outputsheet.newChart().asLineChart();
+    // Draw a basic Squat progress chart
+    let chartSquatRange = outputsheet.getRange('A:B');
 
-        let chart = lineChartBuilder
+    // Look for existing Squat Progress sheet and delete it
+    let squatSheet = ss.getSheetByName("Squat Progress");
+    if (squatSheet) ss.deleteSheet(squatSheet);    
+      
+    let chart = outputsheet.newChart().asLineChart()
           .addRange(chartSquatRange)
           .setTitle('Squat Progress')
           .setXAxisTitle('Date')
           .setYAxisTitle('Epley One Rep Max')
+          .setOption('interpolateNulls', true)
           .setPosition(1, 8, 0, 0)
           .setOption('height', 600)
           .setOption('width', 1000)
@@ -181,17 +214,32 @@ function process_BLOC_data() {
           .setOption('series.0.dataLabel', 'value')
           .build(); 
 
-        outputsheet.insertChart(chart);
+    outputsheet.insertChart(chart);
 
-        bloc_found = true;
-        }
+    // charts in their own objectsheet look big and beautiful 
+    // I have yet to find a way to simply create an objectsheet with a chart
+    // The only way seems to be to make a chart then call moveChartToObjectSheet
+    squatSheet = ss.moveChartToObjectSheet(chart);
+    squatSheet.setName("Squat Progress");
+    squatSheet.activate();
+    ss.setActiveSheet(squatSheet);
+}
+
+// Find the sheet which contains the Squat chart object
+// If it doesn't exist create one
+function getSquatSheet() {
+    let ss = SpreadsheetApp.getActiveSpreadsheet();
+
+    // Loop through all the sheets in the spreadsheet looking for the squat graph
+    let allsheets = ss.getSheets();
+    let squat_found = false;
+
+    for (let s in allsheets) {
+        let sheet = allsheets[s];
+        if (sheet.getSheetName === "Squat Progress") return sheet;
     }
-
-    if (bloc_found) {
-        // FIXME: tell the user the sheet name via the ActiveSpreadsheet?
-        SpreadsheetApp.getUi().alert('Processed BLOC data into new active sheet.'); 
-    } else {
-        SpreadsheetApp.getUi().alert('Did not find BLOC data in any sheets. (be sure to do File->Import the BLOC workout data csv into a sheet of this spreadsheet)');
+    if (!squat_found) {
+      return ss.insertSheet("Squat Progress");
     }
 }
 
